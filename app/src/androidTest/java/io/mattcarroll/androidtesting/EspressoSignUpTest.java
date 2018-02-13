9508928541b58f84f8c21e648e2ae5b4f02b5677
sergio1tsov <mobilotest@gmail.com>
2018-02-12 17:02:27 -0800

    new test cases were added

--- app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignUpTest.java
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignUpTest.java
@@ -1,9 +1,12 @@
 package io.mattcarroll.androidtesting;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import io.mattcarroll.androidtesting.signup.SignUpActivity;
 
@@ -13,7 +16,15 @@ import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static junit.framework.Assert.assertTrue;
 
-public class EspressoSignUpTest {
+@RunWith(AndroidJUnit4.class)
+public class EspressoSignUpTest extends EspressoSignUpPage{
+
+    @Rule
+    public ActivityTestRule<SignUpActivity> activityRule
+            = new ActivityTestRule<>(
+            SignUpActivity.class,
+            true,     // initialTouchMode
+            true);   // launchActivity. False to customize the intent
 
     /**
      * Resource ID guide:
@@ -50,15 +61,56 @@ public class EspressoSignUpTest {
     @Test
     public void userSignUpHappyPath() {
         // TODO implement this test
+
+        // Fill in personal info
+
+        scrollToAndTypeText(R.id.edittext_first_name, "Sergey");
+        scrollToAndTypeText(R.id.edittext_last_name, "Odintsov");
+        scrollToAndTypeText(R.id.edittext_address_line_1, "199 Fremont St");
+        scrollToAndTypeText(R.id.edittext_address_line_2, "Fitbit office");
+        scrollToAndTypeText(R.id.edittext_address_city, "San Francisco");
+        scrollToAndTypeText(R.id.edittext_address_state, "CA");
+        scrollToAndTypeText(R.id.edittext_address_zip, "94105");
+
+        scrollToAndTapOnNextButton();
+
+        assertLandingOnTheCorrectActivity("Programming");
     }
 
     @Test
-    public void requiredFieldsAreRequired() {
+    public void     requiredFieldsAreRequired() {
         // TODO implement this test
+
+        hideKeyboard();
+        scrollToAndTapOnNextButton();
+
+        assertErrorWithTextIsShownOnAField(R.id.edittext_first_name, R.string.input_error_required);
+        assertErrorWithTextIsShownOnAField(R.id.edittext_last_name, R.string.input_error_required);
+        assertErrorWithTextIsShownOnAField(R.id.edittext_address_line_1, R.string.input_error_required);
+        assertErrorWithTextIsShownOnAField(R.id.edittext_address_city, R.string.input_error_required);
+        assertErrorWithTextIsShownOnAField(R.id.edittext_address_state, R.string.input_error_required);
+        assertErrorWithTextIsShownOnAField(R.id.edittext_address_zip, R.string.input_error_required);
     }
 
     @Test
     public void atLeastOneInterestMustBeSelectedToContinue() {
         // TODO implement this test
+
+        // Fill in personal info
+
+        scrollToAndTypeText(R.id.edittext_first_name, "Sergey");
+        scrollToAndTypeText(R.id.edittext_last_name, "Odintsov");
+        scrollToAndTypeText(R.id.edittext_address_line_1, "199 Fremont St");
+        scrollToAndTypeText(R.id.edittext_address_line_2, "Fitbit office");
+        scrollToAndTypeText(R.id.edittext_address_city, "San Francisco");
+        scrollToAndTypeText(R.id.edittext_address_state, "CA");
+        scrollToAndTypeText(R.id.edittext_address_zip, "94105");
+
+        scrollToAndTapOnNextButton();
+
+        // tap next button
+        tapNext();
+
+        assertLandingOnTheCorrectActivity("Please select at least 1 interest.");
     }
 }
